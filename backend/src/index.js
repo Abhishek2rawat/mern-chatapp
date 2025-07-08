7feec0a1f9971fd56b3cbd7a489198c6e2c1ae35
Abhishek2rawat <rawatabhi0406@gmail.com>
2025-07-08 16:10:07 +0530

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,11 +15,12 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(cookieParser());
+
 app.use(cors({
-    origin : "http://localhost:5173",
-    credentials :true,
-})
-);
+  origin: true,
+  credentials: true,
+}));
+
  
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
