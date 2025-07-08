912802f71378af7299ded236567c77ad171e7270
Abhishek2rawat <rawatabhi0406@gmail.com>
2025-07-08 12:15:00 +0530

    manually

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,8 +11,8 @@ import {app , server} from "./lib/socket.js";
 
 dotenv.config();
 
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(cookieParser());
 app.use(cors({
@@ -31,7 +31,26 @@ app.use("/api/messages" , messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/.*/", (req, res) => {
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  // });
+  app.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+
+  app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+
+  app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+  
+  app.get("/profile", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+
+  app.get("/setting", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
